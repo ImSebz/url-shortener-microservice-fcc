679e53cefec5d3c2d2eb36daa2dadc66a0f16518
Sebastián Camargo <juansecapri@hotmail.com>
2024-12-01 18:11:39 +0000

    URL Shortener Microservice solution

--- index.js
+++ index.js
@@ -2,23 +2,67 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient } = require('mongodb');
+const dns = require('dns');
+const urlparser = require('url');
+const { url } = require('inspector');
+const req = require('express/lib/request');
+
+const client = new MongoClient(process.env.MONGO_URI);
+const db = client.db('urlshortener');
+const urls = db.collection('urls');
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function (req, res) {
+  console.log(req.body);
+  const url = req.body.url;
+  const dnslookup = dns.lookup(urlparser.parse(url).hostname,
+    async (err, adress) => {
+      if (!adress) {
+        res.json({ error: 'invalid url' });
+      } else {
+        const urlCount = await urls.countDocuments({});
+        const urlDoc = {
+          url,
+          short_url: urlCount
+        };
+
+        const result = await urls.insertOne(urlDoc);
+        console.log(result);
+        res.json({
+          original_url: url,
+          short_url: urlCount
+        });
+
+      }
+    });
 });
 
-app.listen(port, function() {
+app.get('/api/shorturl/:short_url', async (req, res) => {
+  const short_url = req.params.short_url;
+  const urlDoc = await urls.findOne({ short_url: +short_url });
+
+  if (urlDoc) {
+    res.redirect(urlDoc.url);
+  } else {
+    res.status(404).json({ error: 'invalid url' });
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
